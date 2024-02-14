9fd71427c8394c0be249619a1b8862c0595edc21
TumTum23 <33499460+TumTum23@users.noreply.github.com>
2024-02-14 12:04:28 +0000

    add mock assembly dd contract

---
+++ dd.sol
@@ -0,0 +1,29 @@
+function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    assembly {
+        let ptr := mload(0x40)
+        mstore(ptr, _from)
+        
+        if lt(sload(ptr), _value) {
+            revert(0, 0)
+        }
+        
+        mstore(add(ptr, 0x20), msg.sender)
+        if lt(sload(keccak256(ptr, 0x40)), _value) {
+            revert(0, 0)
+        }
+        
+        sstore(ptr, sub(sload(ptr), _value))
+        sstore(keccak256(ptr, 0x40), sub(sload(keccak256(ptr, 0x40)), _value))
+        
+        mstore(ptr, _to)
+        let recipientBalance := sload(ptr)
+        if lt(sub(0, _value), recipientBalance) {
+            revert(0, 0)
+        }
+        sstore(ptr, add(recipientBalance, _value))
+        
+        mstore(0x40, add(ptr, 0x20))
+        mstore(ptr, 1)
+        return(ptr, 0x20)
+    }
+}
